219649decd58beaaa7962c5d1ccd22bae78936eb
Dinuk1994 <ddt94119@gmail.com>
2024-05-31 21:36:58 +0530

    Initializing Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from '../src/(pages)/home/page'
+import Login from '../src/(pages)/Login/page'
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/home" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
       </Routes>
     </Router>
   )
